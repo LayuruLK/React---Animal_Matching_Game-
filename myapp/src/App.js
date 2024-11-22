d389f7573e09faeb6f68decce63e65d3e67f2c4d
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2024-11-22 21:12:20 +0530

    pass props child to parent

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,11 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import SelectAnimal from './Components/SelectAnimal/SelectAnimal';
 import AnimalName from './Components/AnimalName/AnimalName';
+import { useState } from 'react';
 
 function App() {
+  const [number, setNumber] = useState();
+
+
+  const receiveNum = (num) => {
+    setNumber(num)
+  }
+
   return (
     <div className="App">
       <div className="title">
+      <p>{number}</p>
         <h1>ANIMAL MATCHING GAME</h1>
       </div>
       <div className='container'>
@@ -14,7 +23,8 @@ function App() {
 
         </div>
         <div className="animalName">
-          <AnimalName/>
+          <AnimalName numfun={receiveNum}/>
+          
         </div>
         <div className="animalPhotos">
           <SelectAnimal/>
--- myapp/src/Components/AnimalName/AnimalName.js
+++ myapp/src/Components/AnimalName/AnimalName.js
@@ -1,18 +1,23 @@
 import { useState } from 'react';
 import { animals } from '../../AnimalsDb.js'; 
 
-export default function AnimalName(){
+export default function AnimalName(props){
     const [animalName, setAnimalName] = useState();
+    const [index,setIndex] = useState();
 
     function randomName() {
         let number = Math.floor(Math.random() * 10) + 1;
         let animal = animals[number];
         setAnimalName(animal.name);
+        setIndex(number);
     }
+
+    
     return(
         <>
             <button onClick={() =>{randomName()}}>Start</button>
             <p>{animalName}</p>
+            {props.numfun(index)}
         </>
     );
 }
